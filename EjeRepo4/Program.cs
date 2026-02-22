6a4212dcf07e4a82a40296693535f865b59846f9
chris <chris@LAPTOPCHRIS>
2026-02-22 14:25:11 -0600

    swich_inicio

--- EjeRepo4/Program.cs
+++ EjeRepo4/Program.cs
@@ -1,4 +1,5 @@
 ﻿//Programa con repositorio 4
+int nvlAmenaza = 0;
 Console.WriteLine("Seleccione el incidente ocurrido \n" +
     "1)Malware \n" +
     "2)Phishing \n" +
@@ -21,3 +22,32 @@ Console.WriteLine("La amenaza persiste? (s/n)_");
 string persiste = Console.ReadLine();
 Console.WriteLine("Ingrese el número de usuarios afectados_");
 int usuarios = int.Parse(Console.ReadLine());
+if(persiste == "s" || persiste == "n" && usuarios > 0)
+{
+    switch (incidente)
+    {
+        case 1:
+            switch (device)
+            {
+                case 1:
+                    switch (datos)
+                    {
+                        case 1:
+                             nvlAmenaza += 1;
+                            if(persiste == "s")
+                            {
+                                if(usuarios > 10)
+                                {
+                                    nvlAmenaza += 1;
+                                }
+                            }
+                            break;
+                    }
+                    break;
+            }
+            break;
+    }
+}else
+{
+    Console.WriteLine("Datos invalidos, rebise su respuesta en amenaza existencia y usuarios afectados");
+}
